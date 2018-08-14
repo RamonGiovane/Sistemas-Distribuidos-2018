d051766dcf8b8def34637fd24c2213fc89101990
RamonGiovane <40267373+RamonGiovane@users.noreply.github.com>
2018-08-14 19:09:35 -0300

    ControladorRemoto 1.1
    Novidades:
    - Estabelecido o uso de um arquivo de configuração para clientes definirem quem é o servidor
    - Leves melhorias no código

--- comandos_remotos/cliente.py
+++ comandos_remotos/cliente.py
@@ -1,18 +1,29 @@
 import socket
 
-HOST = '127.0.0.1' # IP do servidor
-PORTA = 15000      # Porta do servidor
+def cliente():
+  arquivo = open('config.txt', 'r')
+  texto = arquivo.readlines()
 
-tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-enderecoServidor  = (HOST, PORTA)
+  #Obtendo os valores de cada parametro do arquivo de config.
+  HOST = texto[0].split(':')[1].strip()
+  PORTA = int(texto[1].split(':')[1].strip())
+  
+  tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+  enderecoServidor  = (HOST, PORTA)
+  tcpSocket.connect(enderecoServidor);
+  print("\tBem-vindo\nComados de mudança de diretório podem não funcionar corretamente.\nUtilize caminhos absolutos")
+  print ("Conexão estabelecida com servidor");
+  while True:
+    comando = input("\n:");
+    if(comando == 'exit'):
+      break
+    tcpSocket.send(comando.encode('utf-8'));
+    print(tcpSocket.recv(1024).decode('utf-8'));
 
-tcpSocket.connect(enderecoServidor);
-print("\tBem-vindo\nComados de mudança de diretório podem não funcionar corretamente.\nUtilize caminhos absolutos")
-print ("Conexão estabelecida com servidor");
-while True:
-  comando = input("\n:");
-  if(comando == 'exit'):
-    break
-  tcpSocket.send(comando.encode('utf-8'));
-  print(tcpSocket.recv(1024).decode('utf-8'));
+try:
+  cliente()
+
+except Exception as e:
+  print("Algo sinistro aconteceu.\nDetalhes do erro: ", e)
+
 
