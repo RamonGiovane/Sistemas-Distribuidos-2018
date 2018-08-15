b3744e76de98c6b37888a5366280e5e370b93bab
RamonGiovane <40267373+RamonGiovane@users.noreply.github.com>
2018-08-15 11:59:15 -0300

    ControladorRemoto 1.2
    Novidades:
    - Quando o servidor se desconecta, os comandos do cliente são executados localmente
    - Melhorias no código
    
    Em desenvolvimento:
    - Cliente tenta se reconectar ao servidor
    
    Ideias:
    - Acrescentar nas configurações um parâmetro de periodicidade para as tentativas de reconexão
    Exemplo: a cada 5 comandos, a cada 20 comandos

--- comandos_remotos/cliente.py
+++ comandos_remotos/cliente.py
@@ -1,6 +1,22 @@
 import socket
+import subprocess
 
-def cliente():
+def executarComandoOffline(comando):
+   output = subprocess.run(comando, shell=True, stdout=subprocess.PIPE, 
+                        universal_newlines=True)
+   
+   resposta = output.stdout
+      
+   if (resposta == '' or resposta == None):
+     if(output.stderr != ''):
+       resposta = output.stderr
+       print (output.stdout)
+       if resposta == None:
+         resposta = '\n'
+                        
+   return resposta
+   
+def conectar():
   arquivo = open('config.txt', 'r')
   texto = arquivo.readlines()
 
@@ -8,22 +24,62 @@ def cliente():
   HOST = texto[0].split(':')[1].strip()
   PORTA = int(texto[1].split(':')[1].strip())
   
-  tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-  enderecoServidor  = (HOST, PORTA)
-  tcpSocket.connect(enderecoServidor);
+  try:
+    tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
+    enderecoServidor  = (HOST, PORTA)
+    tcpSocket.connect(enderecoServidor); 
+    return tcpSocket
+  except Exception as e:
+    return None
+
+def cliente():
   print("\tBem-vindo\nComados de mudança de diretório podem não funcionar corretamente.\nUtilize caminhos absolutos")
-  print ("Conexão estabelecida com servidor");
+  
+  tcpSocket = conectar()
+  if (tcpSocket is None):
+    conexao = False
+  else:
+    print ("Conexão estabelecida com servidor");
+    conexao =  True
+     
+  #Variavel de controle para exibir um aviso quando o servidor desconectar
+  mensagemOffline = True #True significa que a qualquer momento poderá ser exibida
+  
   while True:
     comando = input("\n:");
     if(comando == 'exit'):
       break
-    tcpSocket.send(comando.encode('utf-8'));
-    print(tcpSocket.recv(1024).decode('utf-8'));
+    try:
+      #Tentando reconectar...
+      #if(conexao == False):
+       # tcpSocket = conectar()
+      #  if(tcpSocket):
+      #    print("\nServidor tá de volta, galera!\n")
+      #Está comentado por causar problemas >:( Preciso investigar
+      
+      tcpSocket.send(comando.encode('utf-8'));
+      resposta = tcpSocket.recv(1024).decode('utf-8')
+
+    except:
+      #Fechando o socket
+      if(conexao):
+        tcpSocket.close()
+        conexao = False
+      
+      if(mensagemOffline == True):
+        print("\n!O servidor remoto não está conectado!\n")
+        mensagemOffline = False
+      
+      resposta = executarComandoOffline(comando)
+
+    
+    print(resposta)
 
 try:
   cliente()
 
 except Exception as e:
   print("Algo sinistro aconteceu.\nDetalhes do erro: ", e)
+
 
 
